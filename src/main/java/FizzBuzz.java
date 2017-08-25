095e9bfcd9d84e50cdcaf7d8de04f1b23787f4e2
Meaghan Rea <MeaghanRea@Meaghan-Reas-iMac.local>
2017-08-24 22:21:32 -0400

    got the third test to pass

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,7 +2,11 @@
 public class FizzBuzz {
 
 	public String fizz(Integer inputNumber) {
+		if (inputNumber == 3) {
+			return "Fizz";
+		}
 		return inputNumber.toString();
 	}
 
 }
+ 
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzTest {
@@ -12,6 +13,11 @@ public class FizzBuzzTest {
 	public void whenNumberisTwoOutputShouldBeTwo() {
 		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("2", fizzBuzz.fizz(2));
+	}
+	@Test
+	public void whenNumberisThreeOutputShouldBeFizz() {
+		FizzBuzz fizzBuzz = new FizzBuzz();
+		assertEquals("Fizz", fizzBuzz.fizz(3));
 	}
 }
 
